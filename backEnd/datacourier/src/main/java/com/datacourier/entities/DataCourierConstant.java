2f6c7d3a61ece0cb6381339ad12a84de653689e7
Dilnawaj <48027940+Dilnawaj@users.noreply.github.com>
2024-07-16 11:12:44 +0530

    removed unused files
    removed files

--- backEnd/datacourier/src/main/java/com/datacourier/entities/DataCourierConstant.java
+++
@@ -1,8 +0,0 @@
-package com.datacourier.entities;
-
-public class DataCourierConstant {
-    public static final String USER_EMAIL = "user";
-    public static final String DASH = "-";
-    public static final String UI_URL = "http://localhost:3000/";
-    public static final String LOG_FILE_NAME = "log.log";
-}
--- backEnd/datacourier/src/main/java/com/datacourier/model/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.datacourier.model;
-
-public enum UserType {
-	NORMAL_USER,BOMBIT
-
-}
